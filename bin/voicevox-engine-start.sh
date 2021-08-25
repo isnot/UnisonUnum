5cc31934caea667876b7582eedd8be177850491b
ISHIDA Naoto <naoto@isnot.jp>
2021-08-26 06:19:30 +0900

    fix:

--- bin/voicevox-engine-start.sh
+++ bin/voicevox-engine-start.sh
@@ -5,4 +5,4 @@ WORK_DIR="/home/naoto/repos"
 # 製品版 VOICEVOX でサーバーを起動
 VOICEVOX_DIR=$WORK_DIR"/VOICEVOX-0.4.1" # 製品版 VOICEVOX ディレクトリのパス
 cd $WORK_DIR"/voicevox_engine" # コードがあるディレクトリのパス
-python run.py --voicevox_dir=$VOICEVOX_DIR
+python proxy_run.py --voicevox_dir=$VOICEVOX_DIR
