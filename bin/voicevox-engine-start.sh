0dd5dd67b3e3f924148268fc5450d974363bbc26
ISHIDA Naoto <naoto@isnot.jp>
2021-09-09 13:56:42 +0900

    fix path

--- bin/voicevox-engine-start.sh
+++ bin/voicevox-engine-start.sh
@@ -3,6 +3,6 @@ PWD=`pwd`
 WORK_DIR="/home/naoto/repos"
 
 # 製品版 VOICEVOX でサーバーを起動
-VOICEVOX_DIR=$WORK_DIR"/VOICEVOX-0.4.1" # 製品版 VOICEVOX ディレクトリのパス
+VOICEVOX_DIR=$WORK_DIR"/VOICEVOX" # 製品版 VOICEVOX ディレクトリのパス
 cd $WORK_DIR"/voicevox_engine" # コードがあるディレクトリのパス
 python proxy_run.py --voicevox_dir=$VOICEVOX_DIR
--- bin/voicevox-start.sh
+++ bin/voicevox-start.sh
@@ -3,7 +3,7 @@ PWD=`pwd`
 WORK_DIR="/home/naoto/repos"
 
 # 製品版 VOICEVOX でサーバーを起動
-VOICEVOX_DIR=$WORK_DIR"/VOICEVOX-0.4.1" # 製品版 VOICEVOX ディレクトリのパス
+VOICEVOX_DIR=$WORK_DIR"/VOICEVOX" # 製品版 VOICEVOX ディレクトリのパス
 cd $WORK_DIR"/voicevox_engine" # コードがあるディレクトリのパス
 /usr/bin/bash -c "python run.py --voicevox_dir=$VOICEVOX_DIR &"
 
